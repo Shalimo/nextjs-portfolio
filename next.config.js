16dc0043a7049175436e4f3a4e2e0000c0a7f42e
Nikita Shalimo <shpoisnik@gmail.com>
2022-09-29 00:48:00 +0300

    text x11 for deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {domains: ['localhost', 'nextjs-portfolio-1wj555-nrdbg3k1k-shalimo.vercel.app']},
+  images: {domains: ['localhost', 'nextjs-portfolio-1wj555.vercel.app']},
   env: {
     APP_URL: process.env.REACT_APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
