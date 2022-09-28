dd80e27d6770d6f3259b25c8f46091cef7bc0944
Nikita Shalimo <shpoisnik@gmail.com>
2022-09-29 00:59:49 +0300

    test x12 for deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {domains: ['localhost', 'nextjs-portfolio-1wj555.vercel.app']},
+  images: {
+    domains: [ 'https://nextjs-portfolio-1wj555.vercel.app/', 'http://localhost:3000/']
+},
   env: {
     APP_URL: process.env.REACT_APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
