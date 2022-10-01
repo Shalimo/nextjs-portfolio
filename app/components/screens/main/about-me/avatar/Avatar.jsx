523e891878ac5696cba7969e4e12dfbbb870969f
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-02 01:51:48 +0300

    test x39 for deploy

--- app/components/screens/main/about-me/avatar/Avatar.jsx
+++ app/components/screens/main/about-me/avatar/Avatar.jsx
@@ -4,7 +4,11 @@ import styles from './Avatar.module.scss'
 const Avatar = ({ avatar }) => {
 	return (
 		<div className={styles.image}>
-			<img src="./me.jpg" width="500" heigth="500" />
+			<img
+				src="https://avatars.githubusercontent.com/u/45921781?v=4"
+				width="500"
+				heigth="500"
+			/>
 		</div>
 	)
 }
