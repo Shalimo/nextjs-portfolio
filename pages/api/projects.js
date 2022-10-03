9ed3627bb1bae02dd8a7e5b9fab1305bb119efc2
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-03 19:04:06 +0300

    update next js api

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,99 +1,66 @@
 const links = [
   {
     id: 1,
-    icon: {
-      path: '../../public/icons/CRM.png'
-    },
     url: '',
     title: 'Personal CRM',
     description: 'Commercial project from MVP Connect. React. Working with Firebase, GAPI, People API. Authorization via google and facebook. Layout with dynamic using Figma. Implementation of front-end and back-end communication. Used: context, hooks, custom hooks, axios, JS methods.'
   },
   {
     id: 2,
-    icon: {
-      path: '../../public/icons/terminal.png'
-    },
     url: '',
     title: 'Restaurants Terminal',
     description: 'Commercial project from MVP Connect. React native app. Poster analogue. Layout with dynamic of all screens using Figma. Registration, entering a pin code, adding/deleting an order, menu sorting, adding/deleting guests, choosing a payment method, dropdowns, modals, notifications, booking calendar. Used: context, hooks, custom hooks, JS methods.'
   },
   {
     id: 3,
-    icon: {
-      path: '../../public/icons/github.jpg'
-    },
     url: 'https://github.com/Shalimo/git-mini-paralect',
     title: 'Mini Github',
     description: 'Task project for Paralect. React. Working with GitHub API. Layout, responsive layout. User search. Displaying information about the user and his repositories. Used: hooks, custom hooks, RestAPI, promises, JS methods.'
   },
   {
     id: 4,
-    icon: {
-      path: '../../public/icons/twitter.jpg'
-    },
     url: 'https://github.com/Shalimo/mini-twitter',
     title: 'Mini Twitter',
     description: 'React. Twitter analogue. Adding/deleting posts, ability to like and dislike posts.'
   },
   {
     id: 5,
-    icon: {
-      path: '../../public/icons/pizza.jpg'
-    },
     url: 'https://github.com/Shalimo/restaurant-app-react-redux',
     title: 'Pizza Store',
     description: 'React/Redux. The server is emulated using json database.'
   },
   {
     id: 6,
-    icon: {
-      path: '../../public/icons/food.jpg'
-    },
     url: 'https://github.com/Shalimo/project-food',
     title: 'Food Store',
     description: 'JavaScript. Modules (tabs, modals, timer, calorie calculator, forms, cards), webpack, RestAPI, promises. The server is emulated using json database.'
   },
   {
     id: 7,
-    icon: {
-      path: '../../public/icons/hardware.jpg'
-    },
     url: 'https://github.com/Shalimo/irvas-service',
     title: 'Hardware store',
     description: 'JavaScript. Modules, RestAPI, promises, gulp. The server is emulated using php.'
   },
   {
     id: 8,
-    icon: {
-      path: '../../public/icons/got.jpg'
-    },
     url: 'https://github.com/Shalimo/got-db',
     title: 'Game of Thrones API',
     description: 'React. The universe of the game of thrones database. Working with Game of Thrones API. Used: hooks, RestAPI, promises, JS methods.'
   },
   {
     id: 9,
-    icon: {
-      path: '../../public/icons/converter.jpg'
-    },
     url: 'https://github.com/Shalimo/currency-converter',
     title: 'Currency converter',
     description: 'React. Working with currensy API. Used: RestAPI, promises, hooks.'
   },
   {
     id: 10,
-    icon: {
-      path: '../../public/icons/calendar.jpg'
-    },
     url: 'https://github.com/Shalimo/booking-calendar',
     title: 'Booking calendar',
     description: 'JavaScript. Booking calendar for hotels. Test task.'
   },
   {
     id: 11,
-    icon: {
-      path: '../../public/icons/counter.jpg'
-    },
     url: 'https://github.com/Shalimo/react-redux-counter',
     title: 'Counter',
     description: 'My first project with React/Redux. Just a memory.'
