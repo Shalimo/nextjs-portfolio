8d733f9cb8daa4ecc9542c7630cdd9153d750ed5
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-03 20:02:12 +0300

    update nextjs api

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
     avatar: 'https://avatars.githubusercontent.com/u/45921781?v=4',
     introduction: 
     "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
-    cv: 'https://drive.google.com/file/d/1uXPCQtISJOIvQdi673hMGPjJ78nvDb0b/view?usp=sharing'
+    cv: 'https://drive.google.com/file/d/1hZ5h2lm9QPLbBQdMZ2ZkrMdwv1dDZnnk/view?usp=sharing'
 }
 
 export default function handler(req, res) {
