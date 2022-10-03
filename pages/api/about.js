f72282d8686481d85cde2838c663e08c9f2e10ce
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-03 19:54:47 +0300

    add portfolio to the list of projects and change cv

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
     avatar: 'https://avatars.githubusercontent.com/u/45921781?v=4',
     introduction: 
     "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
-    cv: 'https://drive.google.com/file/d/1e9465NH631RAhFgqr3V0blt3JAwODUT5/view?usp=sharing'
+    cv: 'https://drive.google.com/file/d/1uXPCQtISJOIvQdi673hMGPjJ78nvDb0b/view?usp=sharing'
 }
 
 export default function handler(req, res) {
--- pages/api/projects.js
+++ pages/api/projects.js
@@ -13,54 +13,60 @@ const links = [
   },
   {
     id: 3,
+    url: 'https://github.com/Shalimo/nextjs-portfolio',
+    title: 'Portfolio',
+    description: 'React/NextJS portfolio. Used: nextjs API, RestAPI, hooks, custom hooks, tailwind, Sass Modules, mixins.'
+  },
+  {
+    id: 4,
     url: 'https://github.com/Shalimo/git-mini-paralect',
     title: 'Mini Github',
     description: 'Task project for Paralect. React. Working with GitHub API. Layout, responsive layout. User search. Displaying information about the user and his repositories. Used: hooks, custom hooks, RestAPI, promises, JS methods.'
   },
   {
-    id: 4,
+    id: 5,
     url: 'https://github.com/Shalimo/mini-twitter',
     title: 'Mini Twitter',
     description: 'React. Twitter analogue. Adding/deleting posts, ability to like and dislike posts.'
   },
   {
-    id: 5,
+    id: 6,
     url: 'https://github.com/Shalimo/restaurant-app-react-redux',
     title: 'Pizza Store',
     description: 'React/Redux. The server is emulated using json database.'
   },
   {
-    id: 6,
+    id: 7,
     url: 'https://github.com/Shalimo/project-food',
     title: 'Food Store',
     description: 'JavaScript. Modules (tabs, modals, timer, calorie calculator, forms, cards), webpack, RestAPI, promises. The server is emulated using json database.'
   },
   {
-    id: 7,
+    id: 8,
     url: 'https://github.com/Shalimo/irvas-service',
     title: 'Hardware store',
     description: 'JavaScript. Modules, RestAPI, promises, gulp. The server is emulated using php.'
   },
   {
-    id: 8,
+    id: 9,
     url: 'https://github.com/Shalimo/got-db',
     title: 'Game of Thrones API',
     description: 'React. The universe of the game of thrones database. Working with Game of Thrones API. Used: hooks, RestAPI, promises, JS methods.'
   },
   {
-    id: 9,
+    id: 10,
     url: 'https://github.com/Shalimo/currency-converter',
     title: 'Currency converter',
     description: 'React. Working with currensy API. Used: RestAPI, promises, hooks.'
   },
   {
-    id: 10,
+    id: 11,
     url: 'https://github.com/Shalimo/booking-calendar',
     title: 'Booking calendar',
     description: 'JavaScript. Booking calendar for hotels. Test task.'
   },
   {
-    id: 11,
+    id: 12,
     url: 'https://github.com/Shalimo/react-redux-counter',
     title: 'Counter',
     description: 'My first project with React/Redux. Just a memory.'
