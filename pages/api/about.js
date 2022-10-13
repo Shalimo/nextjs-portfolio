9a79d08f58ed0c500ae4a1683574297a102cb063
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-14 00:40:27 +0300

    update CV

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
     avatar: 'https://avatars.githubusercontent.com/u/45921781?v=4',
     introduction: 
     "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
-    cv: 'https://drive.google.com/file/d/16aoUESJqVRUeZ3Phb_jjpCa7jdAiH77V/view?usp=sharing'
+    cv: 'https://drive.google.com/file/d/1iQlBGWRmLqEObnAbbBxCANo7qHh_ofI7/view'
 }
 
 export default function handler(req, res) {
