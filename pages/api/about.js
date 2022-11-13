336660e53c3d695d15593e34492244186e0380ec
Nikita Shalimo <shpoisnik@gmail.com>
2022-11-13 17:50:30 +0300

    update CV

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
     avatar: 'https://avatars.githubusercontent.com/u/45921781?v=4',
     introduction: 
     "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
-    cv: 'https://drive.google.com/file/d/1iQlBGWRmLqEObnAbbBxCANo7qHh_ofI7/view'
+    cv: 'https://drive.google.com/file/d/1hXQCn5PfdhOCyN_fZjYKtGoY28yRrOg7/view?usp=sharing'
 }
 
 export default function handler(req, res) {
