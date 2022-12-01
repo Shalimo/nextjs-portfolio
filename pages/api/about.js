a7ce17182b8e64139a0ce98b6573cc3380410717
Nikita Shalimo <shpoisnik@gmail.com>
2022-12-01 16:46:55 +0300

    update next api

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
     avatar: 'https://avatars.githubusercontent.com/u/45921781?v=4',
     introduction: 
     "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
-    cv: 'https://drive.google.com/file/d/1Ea6dErTbPit6vEqSpzpD-cDPVbdGl9Gr/view?usp=sharing/'
+    cv: 'https://drive.google.com/file/d/1Ea6dErTbPit6vEqSpzpD-cDPVbdGl9Gr/view?usp=sharing'
 }
 
 export default function handler(req, res) {
