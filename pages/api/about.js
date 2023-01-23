455c7ce56cbe9e85699c0e4bc955290e620d382d
Nikita Shalimo <shpoisnik@gmail.com>
2023-01-24 02:15:40 +0300

    change location

--- pages/api/about.js
+++ pages/api/about.js
@@ -1,7 +1,7 @@
 const about = {
     avatar: 'https://i.ibb.co/ydNqFd3/me.jpg',
     introduction: 
-    "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Minsk</p>",
+        "<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Junior front-end developer based in Warsaw</p>",
     cv: 'https://drive.google.com/file/d/1Ea6dErTbPit6vEqSpzpD-cDPVbdGl9Gr/view?usp=sharing'
 }
 
