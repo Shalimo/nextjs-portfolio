a1e557f11cdb5eaafbb0ca2d14b4570184776f27
Nikita Shalimo <MegaFrostBall@gmail.com>
2023-04-12 13:16:23 +0200

    update CV

--- pages/api/about.js
+++ pages/api/about.js
@@ -2,7 +2,7 @@ const about = {
 	avatar: 'https://i.ibb.co/ydNqFd3/me.jpg',
 	introduction:
 		"<p>Hello, i'm</p><p>Nikita Shalimo</p><p>Front-end developer based in Warsaw</p>",
-	cv: 'https://drive.google.com/file/d/17z5JTTFjwWOhBOWDPUMqGUU3rd6Qf33P/view?usp=sharing'
+	cv: 'https://drive.google.com/file/d/1Yodyfnihs_DOZAx6FTow_4-jclsuti7q/view?usp=sharing'
 }
 
 export default function handler(req, res) {
